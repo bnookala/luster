bf6b897109805364fc4c6acd953beaed6e17ae64
Bhargav Nookala <nooknb@gmail.com>
2012-03-15 22:48:48 -0700

    Moving things around/cleaning up

--- app.js
+++ app.js
@@ -1,46 +1,16 @@
-var app = require('http').createServer(requestHandler).listen(1337);
-var fs = require('fs');
-var path = require('path');
+var express = require('express');
+var app = express.createServer().listen(1337);
 var io = require('socket.io').listen(app);
 
-function requestHandler (request, response) {
-	console.log('request starting...');
+app.use(express.static(__dirname + '/static'));
 
-	var filePath = '.' + request.url;
-	if (filePath == './') {
-		filePath = './index.html';
-	}
+app.get('/', function (request, response) {
+	response.render('view/index.html');
+});
 
-	var extname = path.extname(filePath);
-	var contentType = 'text/html';
-
-	switch (extname) {
-		case '.js':
-			contentType = 'text/javascript';
-			break;
-		case '.css':
-			contentType = 'text/css';
-			break;
-	}
-
-	// Check if the file path exists, and run a callback.
-	path.exists(filePath, function (exists) {
-		if (exists) {
-			fs.readFile(filePath, function(error, content) {
-				if (error) {
-					response.writeHead(500);
-					response.end();
-				} else {
-					response.writeHead(200, { 'Content-Type': contentType });
-					response.end(content, 'utf-8');
-				}
-			});
-		} else {
-			response.writeHead(404);
-			response.end();
-		}
-	});
-}
+app.post('/', function (request, response) {
+	console.log(request);
+});
 
 io.sockets.on('connection', function (socket) {
 	var interval = undefined;
